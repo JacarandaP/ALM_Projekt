6401a4b3a292be919d910d212664cc974a214b79
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-05-11 12:06:05 +0200

    put back files

---
+++ src/main/java/com/example/springdocker/model/Test2.java
@@ -0,0 +1,12 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Jacaranda Perez
+ * Date: 2021-05-11
+ * Project: spring-docker-demo
+ */
+
+public class Test2 {
+    private String yo= "yos";
+    private int year = 3;
+}
