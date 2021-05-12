093203fcf8826988ebab9df49cf8d9d58b92c190
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-05-12 14:15:01 +0200

    mymathclass added

---
+++ src/main/java/com/example/springdocker/controller/MyMathCalc.java
@@ -0,0 +1,23 @@
+package com.example.springdocker.controller;
+
+/**
+ * Created by Jacaranda Perez
+ * Date: 2021-05-12
+ * Project: spring-docker-demo
+ */
+
+public class MyMathCalc {
+   private int a, b;
+
+   public int add(int a, int b){
+       return a + b;
+   }
+   public int multiply(int a, int b){
+       return a * b;
+   }
+
+   public int divide(int a, int b){
+       return a/b;
+   }
+
+}
