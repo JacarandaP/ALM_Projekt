fbaadcc5e95b6bff1461db48412459e5fb844782
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-05-19 17:22:03 +0200

    docker-compose file added

--- src/test/java/com/example/springdocker/SpringdockerApplicationTests.java
+++ src/test/java/com/example/springdocker/SpringdockerApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.springdocker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringdockerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+  //  @Test
+  //  void contextLoads() {
+  //  }
 
 }
